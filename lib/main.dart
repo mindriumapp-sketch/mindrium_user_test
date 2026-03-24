9114064a1772e3915ac2db5f4686c935c612fbe9
UBDBD <sehyun712@naver.com>
2026-03-24 13:19:42 +0900

    etc

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
 import 'package:gad_app_team/utils/text_line_material.dart';
 import 'package:provider/provider.dart';
 import 'package:rive/rive.dart';
-
 import 'package:gad_app_team/data/daycounter.dart';
 import 'package:gad_app_team/data/user_provider.dart';
 import 'package:gad_app_team/data/today_task_provider.dart';
 import 'package:gad_app_team/data/apply_solve_provider.dart';
-import 'package:gad_app_team/app.dart'; // Mindrium 전체 라우팅 포함
+import 'package:gad_app_team/app.dart';
 import 'package:gad_app_team/features/screen_time/screen_time_tracker.dart';
 
 /// 🌊 Mindrium 앱 시작점 (Provider 초기화)
